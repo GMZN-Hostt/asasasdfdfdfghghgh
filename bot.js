7b18e548767eb6eb1a9f68634595e9d8d59822ba
asasasdfdfdfghghgh <46293626+asasasdfdfdfghghgh@users.noreply.github.com>
2019-01-06 11:34:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const UserBlocked = new Set();
 const prefix = '*'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Say : *help // *invite ∾", {type: 'PLAYING'});
+  client.user.setActivity("Say : *help || *invite ∾", {type: 'PLAYING'});
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
