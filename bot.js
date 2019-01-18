a0b0c1b77303d9177d2341f0bc0ab01ef16bed10
asasasdfdfdfghghgh <46293626+asasasdfdfdfghghgh@users.noreply.github.com>
2019-01-18 17:31:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const UserBlocked = new Set();
+const antispam = require("discord-anti-spam");
 const prefix = '*'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
