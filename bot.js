a5f58043ee86bac7d472f840b9da97aa0674d005
asasasdfdfdfghghgh <46293626+asasasdfdfdfghghgh@users.noreply.github.com>
2019-02-21 21:45:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,60 +303,62 @@ client.on ("guildMemberRemove", member => {
 //كود التيكت
 
 client.on("message", (message) => {
-    /// ALPHA CODES
-   if (message.content.startsWith("*new")) {     /// ALPHA CODES
-        const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.name}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });    /// ALPHA CODES
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: **تم إنشاء تذكرتك ، #${c.name}.**`);
-            const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-                .addField(`مرحباّ ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم لدينا قريبا للمساعدة.`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error);
-    }
- 
- 
-  if (message.content.startsWith("*close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-        message.channel.send(`*close امامك 10 ثواني لتأكيد غلق التذكرة - قم ب اعادة كتابة الأمر`)
-            .then((m) => {
-                message.channel.awaitMessages(response => response.content === '*confirm', {
-                        max: 1,
-                        time: 10000,
-                        errors: ['time'],
-                    })    /// ALPHA CODES
-                    .then((collected) => {
-                        message.channel.delete();
-                    })    /// ALPHA CODES
-                    .catch(() => {
-                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                            m2.delete();
-                        }, 3000);
-                    });
-            });
-    }
- 
-});
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+      const reason = message.content.split(" ").slice(1).join(" ");
+      if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`هذا السيرفر ليس لديه \`Support Team\` صنع رتبة, لذلك لن يتم فتح التذكرة.\nاذا كنت تمتلك administrator, إنشاء اسم بهذا الاسم بالضبط وإعطائه للمستخدمين الذين يمكنهم مشاهدة التذاكر.`);
+      if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`انت بالفعل لديك تذكره مفتوحه.`);
+      message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+          let role = message.guild.roles.find("name", "Support Team");
+          let role2 = message.guild.roles.find("name", "@everyone");
+          c.overwritePermissions(role, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          c.overwritePermissions(role2, {
+              SEND_MESSAGES: false,
+              READ_MESSAGES: false
+          });
+          c.overwritePermissions(message.author, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          let mrx = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setAuthor(message.author.tag,message.author.avatarURL)
+          .setDescription(`:white_check_mark: تم إنشاء تذكرتك, #${c.name}.`)
+          .setTimestamp()
+          message.channel.sendEmbed(mrx);
+          const embed = new Discord.RichEmbed()
+          .setColor(0xCF40FA)
+          .addField(`مرحباً ${message.author.username}!`, `من فضلك وضح لماذا قمت بفتح التذكره مع بعض التفاصيل. مسؤلي السرفر سوف يكونوا موجودين في اسرع وقت للمساعده.`)
+          .setTimestamp();
+          c.send({ embed: embed });
+      }).catch(console.error);
+  }
+  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+      if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر.`);
+  
+      message.channel.send(`هل أنت متاكد؟ بمجرد تأكيد, لا يمكنك عكس هذا العمل!!\nللتأكيد ، اكتب \`-confirm\`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاؤها.`)
+      .then((m) => {
+        message.channel.awaitMessages(response => response.content === '-confirm', {
+          max: 1,
+          time: 10000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+            message.channel.delete();
+          })
+          .catch(() => {
+            m.edit('انتهى إغلاق التذاكر ، لم يتم إغلاق التذكرة.').then(m2 => {
+                m2.delete();
+            }, 3000);
+          });
+      });
+  }
+  
+  });
 
 //نهاية كود التيكت
 
