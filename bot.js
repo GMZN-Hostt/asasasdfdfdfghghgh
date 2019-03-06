901bd7f9ca3037964a398a3318d80963e76b2de3
asasasdfdfdfghghgh <46293626+asasasdfdfdfghghgh@users.noreply.github.com>
2019-03-06 15:06:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -224,16 +224,16 @@ client.on('message', message => {
 
 //----
 
-client.on("message", message => {
-  if (message.channel.type === "dm") { 
+//client.on("message", message => {
+  //if (message.channel.type === "dm") { 
 
-      message.channel.startTyping(); 
-      setTimeout(() => { 
-        message.channel.stopTyping(); 
-      }, Math.random() * (1 - 3) + 1 * 1000);
+    //  message.channel.startTyping(); 
+      //setTimeout(() => { 
+        //message.channel.stopTyping(); 
+      //}, Math.random() * (1 - 3) + 1 * 1000);
    
-  } 
-}); 
+ // } 
+//}); 
 
 //---
 
