c6db7ff3437c4ee785df278923b0d87c74faca58
asasasdfdfdfghghgh <46293626+asasasdfdfdfghghgh@users.noreply.github.com>
2019-03-30 01:46:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2157,6 +2157,81 @@ client.on('guildDelete', guild => {
  
 //كود الكيك والبان
  
+
+
+Save New Duplicate & Edit Just Text Twitter 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
  
@@ -2188,6 +2263,7 @@ if (!message.guild.member(user)
   .addField("**السبب**", '**[ ' + `${reason}` + ' ]**')
   message.channel.send({
     embed : kickembed
+user.send(`**You were kicked from ${message.guild.name}, staff ping**`)
    
   })
 }
@@ -2223,6 +2299,8 @@ if (message.mentions.users.size < 1) return message.reply("**منشن الشخص
   .addField("**السبب**", '**[ ' + `${reason}` + ' ]**')
   message.channel.send({
     embed : banembed
+user.send(`**You were baned from ${message.guild.name}, staff ping**`)
+
   })
 }
 });
